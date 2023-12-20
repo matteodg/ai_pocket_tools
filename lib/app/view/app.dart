27dc195fe5d444aded93c1c7cd158e6ffdb8af26
Matteo Di Giovinazzo <matteodg@gmail.com>
2023-12-20 17:10:11 +0100

    Remove counter related classes

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,3 @@
-import 'package:ai_pocket_tools/counter/counter.dart';
 import 'package:ai_pocket_tools/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +15,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: Container(),
     );
   }
 }
