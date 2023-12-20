6ae540ce67e6230bdbe1c28d32fa2cd5977bf1a2
Matteo Di Giovinazzo <matteodg@gmail.com>
2023-12-20 17:05:20 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/matteodigiovinazzo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.matteodigiovinazzo.ai_pocket_tools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:ai_pocket_tools/app/app.dart';
+import 'package:ai_pocket_tools/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
